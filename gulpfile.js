3f91333d45660c7429138cd6fa610f7d9ff7d6e0
Manuel Bitto <manuel.bitto@gmail.com>
2015-03-03 13:13:23 +0100

    Adde gulp build for mobile

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,35 @@ var gulp = require('gulp'),
     execsyncs = require('gulp-execsyncs'),
     sequence = require('run-sequence'),
     del = require('del'),
-    clean = require('gulp-clean');
+    clean = require('gulp-clean'),
+    karma = require('karma').server;
 
 gulp.task('requirejs-build', function(){
     return execsyncs({
-        cmd : 'node r.js -o build.js'
+        cmd : 'node r.js -o build.js',
+        callback  : function(res){
+            console.log(res);
+        }
     });
 });
 
 gulp.task('copy', function(){
-    return gulp.src(['assets/*', 'img/*', 'index.html'], { base: './' })
-        .pipe(gulp.dest('mobile-app/www'));
+    return gulp.src([
+        'assets/*',
+        'img/*',
+        'index.html',
+        'style.css',
+        'bower_components/alertify.js/themes/alertify.default.css',
+        'bower_components/alertify.js/themes/alertify.core.css',
+        'bower_components/requirejs/require.js'
+    ], { base: './' })
+        .pipe(gulp.dest('dist/pileup/www'));
 });
 
 gulp.task('copy-build', function(){
     //return gulp.src(['build/main.js', 'build/main.js.map'])
     return gulp.src(['build/main.js'])
-        .pipe(gulp.dest('mobile-app/www'));
+        .pipe(gulp.dest('dist/pileup/www'));
 });
 
 gulp.task('delete-build', function () {
@@ -26,19 +38,33 @@ gulp.task('delete-build', function () {
         .pipe(clean());
 });
 
-gulp.task('default', function () {
-    sequence(['requirejs-build', 'copy'], 'copy-build', 'delete-build');
+gulp.task('test', function (done) {
+    karma.start({
+        configFile: __dirname + '/karma.conf.js',
+        singleRun: true
+    }, done);
+});
+
+gulp.task('phonegap-build', function () {
+    return execsyncs({
+        cmd : 'cd dist/pileup; phonegap run android --verbose',
+        callback  : function(res){
+            console.log(res);
+        }
+    });
 });
 
 gulp.task('clean', function (cb) {
     del([
-        'mobile-app/www/main.js',
-        'mobile-app/www/main.js.map',
-        'mobile-app/www/assets',
-        'mobile-app/www/index.html'
+        'dist/pileup/www/main.js',
+        'dist/pileup/www/main.js.map',
+        'dist/pileup/www/assets',
+        'dist/pileup/www/bower_components',
+        'dist/pileup/www/style.css',
+        'dist/pileup/www/index.html'
     ], cb);
 });
 
-gulp.task('deploy', function () {
-    // TODO deploy with shell command
+gulp.task('default', function () {
+    sequence('test', ['requirejs-build', 'copy'], 'copy-build', 'delete-build', 'phonegap-build');
 });
