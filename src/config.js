ed8509127614c9c05c54d51b772b9c1d74b9b84d
Manuel Bitto <manuel.bitto@gmail.com>
2015-03-20 02:20:31 +0100

    Change config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 define(function() {
     return {
         STARTING_CIRCLES_QUANTITY: 3,
-        SPLASH_SCREEN_DURATION: 3500,
         OUTLINE_CIRCLE_STROKE: 1,
         OUTLINE_CIRCLE_RADIUS: 50,
         NEW_CIRCLE_ANIMATION_SPEED: 500,
